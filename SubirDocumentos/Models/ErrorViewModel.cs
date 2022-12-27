fc096c2337c266a50ef2ddf879170acad528b623
juanupc <jandressalcedo@unicesar.edu.co>
2022-12-27 09:00:16 -0500

    Pagina web consumir API

---
+++ SubirDocumentos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SubirDocumentos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
